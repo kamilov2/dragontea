f651c75b9594bb6e2270bd56a6bdd82bbd3ca7bb
unknown <jasurkamilov7279@gmail.com>
2024-12-06 07:04:21 +0300

    .

--- dragontea/settings.py
+++ dragontea/settings.py
@@ -133,11 +133,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
